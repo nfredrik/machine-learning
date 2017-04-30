1720fb22ca63e8bf54900d5c2401e32a7b655d8c
Povilas Balciunas <balciunas90@gmail.com>
2017-04-30 12:40:28 +0300

    Add script to plot generated data

---
+++ unlabeled-coordinates/src/plot_data.py
@@ -0,0 +1,27 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+import click
+
+import fs
+
+
+@click.command()
+@click.option('--in', 'data_file', default='vectors.json',
+              help='Input data file path.')
+@click.option('--out', 'output_image', default='data.svg',
+              help='Output image file.')
+def main(data_file: str, output_image: str) -> None:
+    coords = fs.read_json_from(data_file)
+    generate_image_from(coords, output_image)
+
+
+def generate_image_from(coords: list, img_path: str) -> None:
+    ax = plt.subplot(111)
+    for c in coords:
+        ax.plot(c['x'], c['y'], 'o')
+    plt.savefig(img_path)
+
+
+if __name__ == '__main__':
+    main()
